f440ac1663d62adf1277234abaa3df5715bf1e4d
Grzegorz Dembowski <dembowski91@gmail.com>
2014-04-28 10:33:37 +0200

    added tests

---
+++ FacebookMusicApp/UnitTestingProject.Tests/ControlerIndexTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestingProject.Tests
+{
+    [TestClass]
+    public class ControlerIndexTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
