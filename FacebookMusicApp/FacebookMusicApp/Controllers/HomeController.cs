3c2530ae0ad06780005e8aabe73028d127233828
Grzegorz Dembowski <dembowski91@gmail.com>
2014-05-05 09:55:51 +0200

    Prepared Models.
    Signed-off-by: Grzegorz Dembowski <dembowski91@gmail.com>

--- FacebookMusicApp/FacebookMusicApp/Controllers/HomeController.cs
+++ FacebookMusicApp/FacebookMusicApp/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Mvc.Facebook;
 using Microsoft.AspNet.Mvc.Facebook.Client;
 using FacebookMusicApp.Models;
+using System.Web;
+using DayPilot.Web.Mvc;
+using DayPilot.Web.Mvc.Events.Calendar;
 
 namespace FacebookMusicApp.Controllers
 {
@@ -13,6 +16,16 @@ namespace FacebookMusicApp.Controllers
         [FacebookAuthorize("email", "user_photos")]
         public async Task<ActionResult> Index(FacebookContext context)
         {
+            using (var ctx = new FBBDContext())
+            {
+                User nUser = new User() { NameAndSurname = "Pablo Pable"};
+
+                ctx.User.Add(nUser);
+                ctx.SaveChanges();
+
+            }
+
+
             if (ModelState.IsValid)
             {
                 var user = await context.Client.GetCurrentUserAsync<MyAppUser>();
@@ -33,6 +46,26 @@ namespace FacebookMusicApp.Controllers
             }
 
             return View("Error");
+        }
+        public ActionResult Backend()
+        {
+            return new Dpc().CallBack(this);
+        }
+        class Dpc : DayPilotCalendar
+        {
+
+            protected override void OnInit(InitArgs e)
+            {
+                var db = new FBBDContext();
+               // Events = from ev in db.Callendar select ev;
+
+                DataIdField = "id";
+                DataTextField = "text";
+                DataStartField = "eventstart";
+                DataEndField = "eventend";
+
+                Update();
+            }
         }
     }
 }
